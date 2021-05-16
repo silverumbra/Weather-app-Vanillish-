383fd67625ffe88fc3392fa4d9a5bb52a1676c35
silverumbra <83974058+silverumbra@users.noreply.github.com>
2021-05-16 16:17:53 +0100

    created display temperature function. console.log to test

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+
+function displayTemperature(response) {
+    console.log(response.data);
+}
+
 let apiKey = "edb378f0be539a4b2c5c96da53e9f0c1";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
