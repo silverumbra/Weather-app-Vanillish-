d02d7fd62f2a1f9ef244495b6f4ecc04c237e692
silverumbra <83974058+silverumbra@users.noreply.github.com>
2021-05-16 17:10:32 +0100

    fixed hours

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ function formatDate(timestamp) {
     // calculate the date 
     let date = new Date(timestamp);
     let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
     let minutes = date.getMinutes();
     if (minutes < 10) {
         minutes = `0${minutes}`;
