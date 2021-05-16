64b0eb9aab042d078247dce74c7f8a0e4d34cab4
silverumbra <83974058+silverumbra@users.noreply.github.com>
2021-05-16 16:18:56 +0100

    console.log test failed, get(url) url not defined changed to get (apiUrl)

--- src/app.js
+++ src/app.js
@@ -7,4 +7,4 @@ let apiKey = "edb378f0be539a4b2c5c96da53e9f0c1";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 
-axios.get(url).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
