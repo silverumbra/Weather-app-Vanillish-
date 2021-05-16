a40a38ff3d63d66b42c06cddd05e4bd7db45a461
silverumbra <83974058+silverumbra@users.noreply.github.com>
2021-05-16 17:09:14 +0100

    fixed minutes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ function formatDate(timestamp) {
     let date = new Date(timestamp);
     let hours = date.getHours();
     let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if (minutes > 10) {
+        minutes = `0${minutes}`;
+    }
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
