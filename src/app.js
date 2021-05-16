9544e9fa88f43c630d5c8a4da72adf04a621dcf5
silverumbra <83974058+silverumbra@users.noreply.github.com>
2021-05-16 16:16:48 +0100

    removed console.log (api weather testing complete)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 let apiKey = "edb378f0be539a4b2c5c96da53e9f0c1";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+
 axios.get(url).then(displayTemperature);
