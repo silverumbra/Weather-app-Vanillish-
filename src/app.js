3469bf8d1dd1cc7eb815c08032af7db8d08765aa
silverumbra <83974058+silverumbra@users.noreply.github.com>
2021-05-16 16:32:40 +0100

    created cityElement commanded to find the name of the city from weather api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 
 function displayTemperature(response) {
-    console.log(response.data);
+    let cityElement = document.querySelector("#city");
     let temperatureElement = document.querySelector("#temperature");
+    cityElement.innerHTML = response.data.name;
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
