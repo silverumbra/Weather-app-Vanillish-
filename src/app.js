09cd0bf99c96433d85081204e39ca31dfc360580
silverumbra <83974058+silverumbra@users.noreply.github.com>
2021-05-16 16:59:10 +0100

    added format date function

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 
+function formatDate(timestamp) {
+    // calculate the date 
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let day = date.getDay();
+    return `${day} ${hours}:${minutes}`;
+    }
+
+
+
 function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
     let temperatureElement = document.querySelector("#temperature");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
     windElement.innerHTML = Math.round(response.data.wind.speed);
     humidityElement.innerHTML = response.data.main.humidity;
     descriptionElement.innerHTML = response.data.weather[0].description;
